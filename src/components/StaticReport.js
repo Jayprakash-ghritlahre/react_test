e113eb031f3ca482473a9fe80fe03f294612b8ca
Jai Prakash Ghritlahre <jprakash@liquid.diamonds>
2023-02-08 17:43:54 +0000

    Update StaticReport.js

--- src/components/StaticReport.js
+++ src/components/StaticReport.js
@@ -1,64 +1,69 @@
-import React, {Component} from 'react';
-import Table from 'react-bootstrap/Table';
-import { ColorRing } from  'react-loader-spinner'
-
+import React, { Component } from "react";
+import Table from "react-bootstrap/Table";
+import { ColorRing } from "react-loader-spinner";
 
 class StaticReport extends Component {
-  render(){
-  const { DataisLoaded, items } = this.props;
+  render() {
+    const { DataisLoaded, items } = this.props;
 
-  if (!DataisLoaded)
-    return(
-      <div className='row'>
-        <ColorRing
-          visible={true}
-          height="60"
-          width="60"
-          ariaLabel="blocks-loading"
-          wrapperStyle={{}}
-          wrapperClass="blocks-wrapper"
-          colors={['#00319f', '#2e56b1', '#5c7bc2', '#8aa1d3', '#b2c1e2']}
-        />
-        <div className="align-items-center justify-content-center">
-          <h3 className=" text-center"> Please wait... </h3>
+    if (!DataisLoaded)
+      return (
+        <div className="row">
+          <ColorRing
+            visible={true}
+            height="60"
+            width="60"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["#00319f", "#2e56b1", "#5c7bc2", "#8aa1d3", "#b2c1e2"]}
+          />
+          <div className="align-items-center justify-content-center">
+            <h3 className=" text-center"> Please wait... </h3>
+          </div>
         </div>
-      </div>)
+      );
 
     return (
       <div className="row">
         {Object.entries(items).map(([key, value]) => {
-        return (
-          <div className="col-sm-3">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{key.toUpperCase()}</h5>
-                <table className="table table-sm table-hover">
-                  <thead>
-                    <tr>
-                      <th scope="col">Name</th>
-                      <th scope="col">Count</th>
-                      <th scope="col">Value</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {value.map(element => {
-                    return(
+          return (
+            <div className="col-sm-3">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{key.toUpperCase()}</h5>
+                  <table className="table table-sm table-hover">
+                    <thead>
                       <tr>
-                        <td scope='row' style={{textAlign: 'left'}}>{element.name}</td>
-                        <td scope='row'>{element.count}</td>
-                        <td scope='row'>{element.total_amount.slice(0,-3)}</td>
+                        <th scope="col">Name</th>
+                        <th scope="col">Count</th>
+                        <th scope="col">Value</th>
                       </tr>
-                    )})}
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody>
+                      {value.map((element) => {
+                        return (
+                          <tr>
+                            <td scope="row" style={{ textAlign: "left" }}>
+                              {element.name}
+                            </td>
+                            <td scope="row">{element.count}</td>
+                            <td scope="row" style={{ backgroundColor: "red" }}>
+                              {element.total_amount.slice(0, -3)}
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </div>
               </div>
-            </div>      
-          </div>
-        )})}
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
 export default StaticReport;
-
